4bc5c39de47451f71e1aa221f20e92668e272510
DTGriffiths <rhys.griffiths_@outlook.com>
2026-09-10 00:56:55 +0100

    Add block_letters.py to print ASCII letters
    Print J and D in ASCII block letters.

---
+++ learn_python_3/hello_world/block_letters.py
@@ -0,0 +1,19 @@
+# Joe Doe
+# This is a false name
+# Print ASCII J
+print('''JJJJJ
+  J
+  J
+  J
+J J
+J J
+ JJ''')
+# Print ASCII D
+print('''DDDD
+D    D
+D    D
+D    D
+D    D
+D    D
+DDDD
+''')
